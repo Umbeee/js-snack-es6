8b687d85ca037ede838d99e564c24f601892f5e4
Umbeee <umberto.nesta@hotmail.com>
2023-04-16 18:55:38 +0200

    creazione repo + array

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+const bici = [
+    {
+        nome: 'Legnano',
+        peso: 7.2,
+    },
+    {
+        nome: 'Pinarello',
+        peso: 6.85,
+    },
+    {
+        nome: 'Cannondale',
+        peso: 8.3,
+    }
+]
+
+console.log(bici)
