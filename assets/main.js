94878aafc953ba48ff478061c03c5c85b76c2a70
Umbeee <umberto.nesta@hotmail.com>
2023-04-20 01:54:20 +0200

    da finire

--- assets/main.js
+++ assets/main.js
@@ -13,4 +13,34 @@ const bici = [
     }
 ]
 
+const divBici = document.querySelector('.container-bici')
+
+
+
 console.log(bici)
+
+let min = 100;
+let minNome = ''
+
+let max = 0;
+let maxNome = ''
+
+
+
+for (let i=0; i<bici.length; i++){
+
+    if (bici[i]['peso']<min){
+        min = bici[i]['peso']
+        minNome = bici[i]['nome']
+    }
+    if(bici[i]['peso']>max){
+        max = bici[i]['peso']
+        maxNome = bici[i]['nome']
+    }
+}
+
+console.log(min, max)
+
+divBici.innerHTML += `<p>La ${minNome}, è la più leggera, pesando solo ${min} kg</p>`
+
+divBici.innerHTML += `<p>La ${maxNome}, è la più leggera, pesando ${max} kg</p>`
