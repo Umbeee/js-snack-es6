1b126e9435f7015de25aead4b39473c6ff2c2578
Umbeee <umberto.nesta@hotmail.com>
2023-04-25 20:43:27 +0200

    snack 2 finito, da rivedere correzione

--- assets/main.js
+++ assets/main.js
@@ -43,4 +43,60 @@ console.log(min, max)
 
 divBici.innerHTML += `<p>La ${minNome}, è la più leggera, pesando solo ${min} kg</p>`
 
-divBici.innerHTML += `<p>La ${maxNome}, è la più leggera, pesando ${max} kg</p>`
+divBici.innerHTML += `<p>La ${maxNome}, è la più pesante, pesando ${max} kg</p>`
+
+
+const squadre = [
+    {
+        nome: 'juventus',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'milan',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'inter',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'roma',
+        punti: 0,
+        falli: 0,
+    }
+]
+
+function numRandom(min, max){
+    return Math.floor(Math.random() * max) + min
+}
+
+for (let i=0; i<squadre.length; i++){
+
+
+    squadre[i].punti = numRandom(30, 90)
+
+    squadre[i].falli = numRandom(5, 25)
+
+    
+}
+
+console.log(squadre)
+
+
+let nomi = []
+for( let i=0; i<squadre.length; i++){
+    nomi.push(squadre[i].nome + ' ' + squadre[i].falli)
+}
+
+let falli = []
+for( let i=0; i<squadre.length; i++){
+    falli.push(squadre[i].falli)
+}
+
+
+console.log(nomi, falli)
+
+
